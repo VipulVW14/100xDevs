114e6eda6b9a8c11247a1dc9458a57b3eb853dff
dee022 <133804910+dee022@users.noreply.github.com>
2023-09-19 01:19:33 +0530

    videocard

--- week12.2-yt_clone/src/components/VideoCard.tsx
+++ week12.2-yt_clone/src/components/VideoCard.tsx
@@ -1,16 +1,22 @@
 export function VideoCard(){
     return(
         <div>
-             <img src="https://picsum.photos/400/200" alt="" />
-             <div className="grid grid-cols-12">
+             <img src="https://picsum.photos/400/200" className="rounded-md" />
+             <div className="grid grid-cols-12 pt-2">
                 <div className="col-span-1">
-                    <img className={"rounded-full w-20 h-20"} src="https://picsum.photos/30/30" alt="" />
+                    <img className={"rounded-full w-12 h-12"} src="https://picsum.photos/30/30" alt="" />
                 </div>
-                <div className="col-span-10">
-                    Jawan: Chaleya(Hindi) | Shahrukh Khan
-                </div>
-                <div className="col-span-10">
-                    Jawan: Chaleya(Hindi) | Shahrukh Khan
+
+                <div className="col-span-11 ">
+                    <div>
+                        Jawan: Chaleya (Hindi) | Shahrukh Khan
+                    </div>
+                    <div className="col-span-11 text-gray-400 text-base">
+                        Harkirat Singh
+                    </div>
+                    <div className="col-span-11 text-gray-400 text-base">
+                        46M views | 5 months ago
+                    </div>
                 </div>
              </div>
         </div>
